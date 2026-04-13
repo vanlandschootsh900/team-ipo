c5c3e4482535a45e023422df43c8040e809f515a
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-04-13 10:12:29 -0400

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,7 +1,3 @@
-
-
-
-
 //Intput
 const test_1 = 48;
 
@@ -15,9 +11,6 @@ const test_3 = 95;
 const avg_test = (test_1 + test_2 + test_3)/3;
 
 //output
-const output = (`Test score 1 ${test_1} 
-    Test score 2 ${test_2}
-    Test score 3 ${test_3}
-    the avg is ${avg_test}`);
+const output = (`Test score 1 ${test_1}, Test score 2 ${test_2}, Test score 3 ${test_3}, the avg is ${avg_test}`);
 
-console.log(output)
+console.log(output);
